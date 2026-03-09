7d0cd1dc316f600055ccc9e4d7f9b61776642845
A1exSid <alexsiboleg@gmail.com>
2026-03-09 11:55:50 +0700

    doftjnmgnfg

---
+++ Assets/SMERT.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DestroyOnFall : MonoBehaviour
+{
+    // Установите минимальную высоту, ниже которой объект будет уничтожаться
+    public float fallThreshold = -5f;
+
+    void Update()
+    {
+        // Проверяем, если объект ниже пороговой высоты
+        if (transform.position.y < fallThreshold)
+        {
+            Destroy(gameObject); // Уничтожаем объект
+        }
+    }
+}
