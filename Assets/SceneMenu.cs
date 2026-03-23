d79cf580c2691d3b6bab7ef6a36b758fb7d933db
A1exSid <alexsiboleg@gmail.com>
2026-03-23 11:41:19 +0700

    ggg

---
+++ Assets/SceneMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class SceneMenu : MonoBehaviour
+{
+       public void SceneLoader(string levels)
+    {
+        SceneManager.LoadScene("levels");
+    }
+
+
+
+}
+   
